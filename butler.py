96cdf278f5edc6b26410404b5813e21a01fb280c
Les Pounder <lespounder@gmail.com>
2017-01-21 11:19:02 +0000

    Updated to add PIR sensor

--- butler.py
+++ butler.py
@@ -1,40 +1,44 @@
+from gpiozero import MotionSensor
 import feedparser
 import time
 from gtts import gTTS
 import os
 import pyowm
 news = feedparser.parse("http://feeds.bbci.co.uk/news/rss.xml?edition=uk")
+pir = MotionSensor(17)
 stories = []
 
-print(news['feed']['title'])
+while True:
+    status = pir.wait_for_inactive()
+    if status == True:
+        print(news['feed']['title'])
 
-current_time = time.ctime()
-str(current_time)
-print(current_time)
-tts = gTTS(text=(current_time), lang='en-us')
-tts.save("time.mp3")
-os.system("mpg321 time.mp3")
+        current_time = time.ctime()
+        str(current_time)
+        print(current_time)
+        tts = gTTS(text=(current_time), lang='en-us')
+        tts.save("time.mp3")
+        os.system("mpg321 time.mp3")
 
-for i in range(3):
-    tts = gTTS(text=(news['entries'][i]['title']), lang='en-us')
-    tts.save("news.mp3")
-    os.system("mpg321 news.mp3")
+        for i in range(3):
+            tts = gTTS(text=(news['entries'][i]['title']), lang='en-us')
+            tts.save("news.mp3")
+            os.system("mpg321 news.mp3")
 
 
-owm = pyowm.OWM('API KEY')
-observation = owm.weather_at_place("Blackpool,uk")
-w = observation.get_weather()
+        owm = pyowm.OWM('API KEY')
+        observation = owm.weather_at_place("Blackpool,uk")
+        w = observation.get_weather()
 
-weather = w.get_status()
-tts = gTTS(text=("The current weather is, "+str(weather)), lang='en-us')
-tts.save("weather.mp3")
-os.system("mpg321 weather.mp3")
+        weather = w.get_status()
+        tts = gTTS(text=("The current weather is, "+str(weather)), lang='en-us')
+        tts.save("weather.mp3")
+        os.system("mpg321 weather.mp3")
 
-temperature = w.get_temperature('celsius')['temp_max']
-tts = gTTS(text=("The current temperature is, "+str(temperature)+"celsius"), lang='en-us')
-tts.save("temperature.mp3")
-os.system("mpg321 temperature.mp3")
+        temperature = w.get_temperature('celsius')['temp_max']
+        tts = gTTS(text=("The current temperature is, "+str(temperature)+"celsius"), lang='en-us')
+        tts.save("temperature.mp3")
+        os.system("mpg321 temperature.mp3")
 
-print("The current weather is, ",w.get_status())
-print("The current temperature is, ",w.get_temperature('celsius')['temp_max']
-)
+        print("The current weather is, ",w.get_status())
+        print("The current temperature is, ",w.get_temperature('celsius')['temp_max'])
