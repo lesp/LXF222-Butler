4c5f993da8585bd74d59e0fffbd8f8e67e197eb3
Les Pounder <lespounder@gmail.com>
2017-01-21 16:55:05 +0000

    updated the code

--- butler.py
+++ butler.py
@@ -19,21 +19,21 @@ while True:
         os.system("mpg321 time.mp3")
 
         for i in range(5):
-            tts = gTTS(text=(news['entries'][i]['title']), lang='en-us')
+            tts = gTTS(text=(news["entries"][i]["title"]), lang="en-us")
             tts.save("news.mp3")
             os.system("mpg321 news.mp3")
 
 
-        owm = pyowm.OWM('77bcfa639fe392ddcdcf102938a2d3b5')
+        owm = pyowm.OWM("77bcfa639fe392ddcdcf102938a2d3b5")
         observation = owm.weather_at_place("Blackpool,uk")
         w = observation.get_weather()
 
         weather = w.get_status()
-        tts = gTTS(text=("The current weather is, "+str(weather)), lang='en-us')
+        tts = gTTS(text=("The current weather is, "+str(weather)), lang="en-us")
         tts.save("weather.mp3")
         os.system("mpg321 weather.mp3")
 
-        temperature = w.get_temperature('celsius')['temp_max']
-        tts = gTTS(text=("The current temperature is, "+str(temperature)+"celsius"), lang='en-us')
+        temperature = w.get_temperature("celsius")["temp_max"]
+        tts = gTTS(text=("The current temperature is, "+str(temperature)+"celsius"), lang="en-us")
         tts.save("temperature.mp3")
         os.system("mpg321 temperature.mp3")
